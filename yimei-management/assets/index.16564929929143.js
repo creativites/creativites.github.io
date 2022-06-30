76f305c7c26fc5c49f441a2c774b78da6304d5f4
creativites <1015828419@qq.com>
2022-06-30 14:36:23 +0800

    更新

---
+++ yimei-management/assets/index.16564929929143.js
@@ -0,0 +1,1 @@
+import{V as t}from"./index.1656492992914.js";function n(o){return t({url:"/login",method:"post",data:o})}function e(o){return t({url:"/yimei/logout",method:"post",data:o})}export{e as l,n as s};
